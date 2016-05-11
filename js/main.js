2b6387c671eb72f633cfefa7b9c47292fb26c3b6
BuonOmo <buonomo.ulysse@gmail.com>
2016-05-11 11:21:36 +0200

    export to csv #2

--- js/main.js
+++ js/main.js
@@ -12,8 +12,7 @@ $('#create').click(function () {
   	"impro_type":        $('#impro_type').val()
   }
   collection.push(el);
-  writeCollection();
-  printElement(el);
+  refreshCollection();
 });
 $('#change').click(function () {
   var el = {
@@ -55,6 +54,7 @@ function refreshCollection () {
     printElement(collection[i]);
   }
   writeCollection();
+  exportCollection();
 }
 
 /**
@@ -101,4 +101,62 @@ function writeCollection () {
   var file=new File([str],"collection.json");
   var url=URL.createObjectURL(file);
   $('#save').attr('href',url);
+}
+
+function exportCollection () {
+  var str=collectionToCsv();
+  var file=new File([str],"collection.csv");
+  var url=URL.createObjectURL(file);
+  $('#export').attr('href',url);
+}
+/****************************  CSV conversion  ***************************/
+
+/**
+ * take a json impro and convert it to csv
+ * @param  object el impro json
+ * @return string    csv formatted value
+ * type;;joueurs;
+ * ;theme;;
+ * ;;categorie;
+ */
+function toCsv (el) {
+  var str = "";
+  str+= el.impro_type+";;"+el.number_of_players+";\n"
+  str+= ";"+el.theme+";;\n";
+  str+= ";;"+el.category+";\n";
+  return str;
+}
+
+/**
+ * take two json impro and convert it to csv
+ * @param  object el1 impro json
+ * @param  object el2 impro json
+ * @return string     csv formatted value
+ * type;;joueurs;;;type;;joueurs;
+ * ;theme;;;;;theme;;
+ * ;;categorie;;;;;categorie;
+ */
+function toCsv (el1,el2) {
+  var str = "";
+  str+= el1.impro_type+";;"+el1.number_of_players+";;;"+el2.impro_type+";;"+el2.number_of_players+";\n"
+  str+= ";"+el1.theme+";;;;"+";"+el2.theme+";;\n";
+  str+= ";;"+el1.category+";;;"+";;"+el2.category+";\n";
+  return str;
+}
+
+/**
+ * All the collection to csv
+ * @return str csv file
+ */
+function collectionToCsv () {
+  var str = "";
+  for (var i = 0; i < collection.length; i++) {
+    if (typeof(collection[i+1]) != 'undefined'){
+      str+= toCsv(collection[i],collection[i++]);
+    } else {
+      str+=toCsv(collection[i]);
+    }
+    str+="\n\n";
+  }
+  return str;
 }
